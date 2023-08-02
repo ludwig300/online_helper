d2d32279eb317fd79ef3fd90480763e8a089baa1
ludwig300 <mr4silent@gmail.com>
2023-08-02 21:26:41 +0700

    setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,41 +1,56 @@
 import os
 
 from dotenv import load_dotenv
+from google.cloud import dialogflow
 from telegram import Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, Updater)
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    """Отправляет сообщение при команде /start."""
     update.message.reply_text('Здравствуйте!')
 
 
+def detect_intent_texts(project_id, session_id, text, language_code, update: Update, context: CallbackContext):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    # Отправляем пользователю Telegram ответ бота DialogFlow
+    update.message.reply_text(response.query_result.fulfillment_text)
+
+
 def echo(update: Update, context: CallbackContext) -> None:
-    """Эхо функция: повторяет сообщение пользователя."""
-    update.message.reply_text(update.message.text)
+    """Эхо функция: отправляет сообщение пользователя в DialogFlow."""
+    project_id = os.getenv('DF_PROJECT_ID')
+    session_id = str(update.effective_user.id)
+    language_code = "ru"
+    text = update.message.text
+    detect_intent_texts(project_id, session_id, text, language_code, update, context)
 
 
 def main() -> None:
     load_dotenv()
     tg_token = os.getenv('TG_TOKEN')
-    """Запуск бота."""
+
     updater = Updater(tg_token, use_context=True)
 
     dispatcher = updater.dispatcher
 
-    # обработчик команды /start
     dispatcher.add_handler(CommandHandler('start', start))
 
-    # обработчик всех текстовых сообщений
     dispatcher.add_handler(
         MessageHandler(Filters.text & ~Filters.command, echo)
     )
 
-    # начинаем поиск обновлений
     updater.start_polling()
 
-    # работаем до тех пор, пока бот не будет остановлен
     updater.idle()
 
 
